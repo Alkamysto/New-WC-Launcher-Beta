8457c56bd563c4e541d0023ed3b50ce7889fcacb
Walou41 <165910794+Walou41@users.noreply.github.com>
2025-10-21 02:34:26 +0200

    [TEST] fix maintenance mode

--- src/assets/js/utils/config.js
+++ src/assets/js/utils/config.js
@@ -5,6 +5,7 @@
 
 const pkg = require('../package.json');
 const convert = require('xml-js');
+const nodeFetch = require("node-fetch");
 let url = pkg.user ? `${pkg.url}/${pkg.user}` : pkg.url
 
 let config = `${url}/launcher/config-launcher/config.json`;
@@ -13,7 +14,7 @@ let news = `${url}/launcher/news-launcher/news.json`;
 class Config {
     GetConfig() {
         return new Promise((resolve, reject) => {
-            fetch(config).then(async config => {
+            nodefetch(config).then(async config => {
                 if(config.status === 200) return resolve(config.json());
                 else return reject({ error: { code: config.statusText, message: 'Server not accessible' } });
             }).catch(error => {
